767f47278af9c3d76ea55e9c45954efe308b1987
experimentingwithcode <sohamdhar@gmail.com>
2026-06-04 10:44:45 -0400

    Add voice constants for text-to-speech service

---
+++ ai-service/app/constants/voices.py
@@ -0,0 +1,38 @@
+DEFAULT_VOICE = {
+    "id": "us_neutral",
+    "name": "US English (Default)",
+    "locale": "en-US",
+    "style": "neutral"
+}
+
+VOICES = [
+    DEFAULT_VOICE,
+
+    {
+        "id": "us_female_warm",
+        "name": "US English - Warm Female",
+        "locale": "en-US",
+        "style": "warm"
+    },
+
+    {
+        "id": "us_male_broadcaster",
+        "name": "US English - Broadcaster",
+        "locale": "en-US",
+        "style": "commentator"
+    },
+
+    {
+        "id": "uk_posh",
+        "name": "UK English - Posh",
+        "locale": "en-GB",
+        "style": "formal"
+    },
+
+    {
+        "id": "spooky",
+        "name": "Spooky Voice",
+        "locale": "en-US",
+        "style": "dramatic"
+    }
+]
